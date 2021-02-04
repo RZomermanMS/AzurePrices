1aefe37c4977caa3e4b134a6ebc3c7615db3b5aa
Roelf Zomerman <rozome@microsoft.com>
2021-02-04 14:54:05 +0400

    Update AzurePrices.psm1

--- AzurePrices.psm1
+++ AzurePrices.psm1
@@ -14,9 +14,10 @@ Function Get-AZVMPrices{
         $login=$false
     )  
 
-    if ($subscription){
+    if ($LimitTosubscriptionID){
         write-host "Subscription target= $LimitTosubscriptionID"
         $AvailableSizes=Show-VMsForSubscription -Location $Location -subscriptionID $LimitTosubscriptionID -login $login
+        write-host ("Received " + $AvailableSizes.count + " sizes from scubscription in " + $Location + " region")
     }
 
     $URI="https://prices.azure.com/api/retail/prices?`$filter=armRegionName%20eq%20%27" + $location + "%27%20and%20serviceName%20eq%20%27Virtual%20Machines%27"
@@ -55,15 +56,10 @@ Function Get-AZVMPrices{
         $ObjectsClean=$ObjectsClean | Where-Object -FilterScript {$VMNames -contains $_.armSkuName}
     }
     
-
-    
-    $VM=$ObjectsClean | where {$_.serviceName -eq 'Virtual Machines'}
-    $VMsInLocation=$VM | where {$_.armRegionName -eq $location}
-
     ## SORT THEM TO PAYG, 1YR, 3YR
-    $VmsPAYG = $VMsInLocation | where {!($_.reservationTerm)}
-    $VMs1Yr= $VMsInLocation | where {$_.reservationTerm -eq '1 Year'}
-    $VMs3Yr= $VMsInLocation | where {$_.reservationTerm -eq '3 Years'}
+    $VmsPAYG = $ObjectsClean | where {!($_.reservationTerm)}
+    $VMs1Yr= $ObjectsClean | where {$_.reservationTerm -eq '1 Year'}
+    $VMs3Yr= $ObjectsClean | where {$_.reservationTerm -eq '3 Years'}
 
     ## CREATE ARRAY FOR PRICES
     [system.collections.arraylist]$outputArray = @()
@@ -72,14 +68,27 @@ Function Get-AZVMPrices{
         $VmMeter=$_.meterName
         $VMSKU=$_.armSkuName
         $VMs1YrPrice=($VMs1Yr | where {$_.meterName -eq $VmMeter}).retailPrice
+        #Fix as some SKU's (DS12, DS13, DS14 are retrieved multiple times - all with same prices)
+        If ($VMs1YrPrice -is [array]) {
+            $VMs1YrPrice=$VMs1YrPrice[0]
+        }
         $VMs3YrPrice=($VMs3Yr | where {$_.meterName -eq $VmMeter}).retailPrice
+        If ($VMs3YrPrice -is [array]) {
+            $VMs3YrPrice=$VMs3YrPrice[0]
+        }
 
         #PAYGACR is with ACR discount
-        $PAYGACR=[math]::Round((($_.retailPrice-($_.retailPrice * ($ARCdiscount/100)))*730),2)
+        If ($ARCdiscount -ne 0) {
+            $PAYGACR=[math]::Round((($_.retailPrice-($_.retailPrice * ($ARCdiscount/100)))*730),2)
+        }else{
+            $PAYGACR=[math]::Round(($_.retailPrice * 730),2)
+        }
+
 
         $PAYGMonth=[math]::Round(($_.retailPrice*730),2)
         $VM1YRMonth=[math]::Round(($VMs1YrPrice/12),2)
         $VM3YRMonth=[math]::Round(($VMs3YrPrice/36),2)
+
         If ($_.type -eq 'DevTestConsumption') {
             $DEV = 'DEV'
         }else{
@@ -94,17 +103,17 @@ Function Get-AZVMPrices{
         If ($LimitTosubscriptionID){
             $Cores=($AvailableSizes | where {$_.Name -eq $VMSKU}).NumberOfCores
             $MEMGB=(($AvailableSizes | where {$_.Name -eq $VMSKU}).MemoryInMB)/1024
-            $_ | Add-Member -MemberType NoteProperty -Name "Cores" -Value $Cores
-            $_ | Add-Member -MemberType NoteProperty -Name "Memory" -Value $MEMGB
+            $_ | Add-Member -MemberType NoteProperty -Name "Cores" -Value $Cores -Force
+            $_ | Add-Member -MemberType NoteProperty -Name "Memory" -Value $MEMGB -Force    
         }
 
-        $_ | Add-Member -MemberType NoteProperty -Name "PAYG" -Value $PAYGMonth
-        $_ | Add-Member -MemberType NoteProperty -Name "Discount" -Value $ARCdiscount
-        $_ | Add-Member -MemberType NoteProperty -Name "DACR" -Value $PAYGACR
-        $_ | Add-Member -MemberType NoteProperty -Name "1YR" -Value $VM1YRMonth
-        $_ | Add-Member -MemberType NoteProperty -Name "3YR" -Value $VM3YRMonth
-        $_ | Add-Member -MemberType NoteProperty -Name "1Y%" -Value $1YrPercentage
-        $_ | Add-Member -MemberType NoteProperty -Name "3Y%" -Value $3YrPercentage
+        $_ | Add-Member -MemberType NoteProperty -Name "PAYG" -Value $PAYGMonth -Force
+        $_ | Add-Member -MemberType NoteProperty -Name "Discount" -Value $ARCdiscount -Force
+        $_ | Add-Member -MemberType NoteProperty -Name "DACR" -Value $PAYGACR -Force
+        $_ | Add-Member -MemberType NoteProperty -Name "1YR" -Value $VM1YRMonth -Force
+        $_ | Add-Member -MemberType NoteProperty -Name "3YR" -Value $VM3YRMonth -Force
+        $_ | Add-Member -MemberType NoteProperty -Name "1Y%" -Value $1YrPercentage -Force
+        $_ | Add-Member -MemberType NoteProperty -Name "3Y%" -Value $3YrPercentage -Force
         
 
     }
@@ -113,6 +122,11 @@ Function Get-AZVMPrices{
     If (!($IncludeDev)){
         $VmsPAYG=$VmsPAYG|where {$_.type -ne 'DevTestConsumption'}
     }
+    write-host '$yourVariable=Get-AZVMPrices -ARCdiscount 10 -Location uaenorth' -ForegroundColor Cyan
+    write-host 'then adjust the output as needed for $MyItems array' -ForegroundColor Cyan
+    write-host 'example: $yourVariable | select meterName, productName, PAYG, DACR, 1YR, 1Y%, 3YR, 3Y% | sort-object -property meterName |FT' -ForegroundColor Yellow
+    write-host 'example: $yourVariable | select meterName, productName, PAYG, DACR, 1YR, 1Y%, 3YR, 3Y% | sort-object -property meterName |Export-CSV c:\myprices.csv' -ForegroundColor Yellow
+    write-host 'example: $yourVariable | where {$_.Cores -eq 4} | ft' -ForegroundColor Yellow
     return $VmsPAYG
 }
 
